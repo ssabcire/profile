efac40808ef2732a74568d12b6467d47b3fc31cc
ssab <qasw4554@gmail.com>
2019-07-14 11:37:13 +0900

    add

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("template/"))
+	mux.Handle("/template/", http.StripPrefix("/template/", files))
+
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
